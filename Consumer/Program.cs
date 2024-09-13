e581feee4bcb5a8760ef75f685cf637bb9bdeb06
marko.vujic <marko.vujic@propertycaptain.ch>
2024-09-13 08:59:43 +0200

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.Received += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Received message: {0}", message);
+};
+
+await channel.BasicConsumeAsync(queue: "letterbox", autoAck: true, consumer: consumer); 
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var message = $"Hello, World! + {RandomNumberGenerator.GetInt32(0, 1500)}";
+
+var encodeMessage = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: "", routingKey: "letterbox", body: new ReadOnlyMemory<byte>(encodeMessage), cancellationToken: CancellationToken.None );
+
+Console.WriteLine("Published message: {0}", message);
